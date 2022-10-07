9b3b118438675db61771f3a391ee89dc224b39f7
Aman Mathur <Amanmathur150@gmail.com>
2022-10-07 21:06:53 +0530

    ready for webhooks hurrey

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -73,7 +73,7 @@ exports.getPaymentCancel = catchAsync( async (req,res,next) =>{
 const createBookingCheckout= async(session)=>{
     console.log(session)
   
-    const user =   await User.findOne({email : session.customer_details.email})._id
+    const user = (await User.findOne({email : session.customer_details.email}))._id
     const tour = session.client_reference_id
     const price = session.amount_total /100
     if (user && tour&& price){
